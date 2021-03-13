75f1dbc8170b8834a92a18713867717172f362b5
aman18327 <aman18327@iiitd.ac.in>
2021-03-13 11:56:28 +0530

    Added HomePage and updated Generic Layout Fixed #2

---
+++ Components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function Cart({ currentUser, setcurrentUser }) {
+    return (
+        <View>
+            <Text>This is Carts View</Text>
+        </View>
+    )
+}
+
+export default Cart
