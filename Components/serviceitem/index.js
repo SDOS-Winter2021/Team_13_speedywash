ba338ffe0abe690b0a294201e43b5b8d33fc19ef
Gavish Gupta <gavish18390@iiitd.ac.in>
2021-03-13 14:22:04 +0530

    added service specific view. Fixed #3

--- Components/serviceitem/index.js
+++ Components/serviceitem/index.js
@@ -3,19 +3,19 @@ import {View,Text, ImageBackground, Pressable,Image} from 'react-native';
 import styles from './styles';
 import ServiceItemType from '../serviceitemtype';
 // import Footer from '../Footer/Footer.js';
-import Header from '../header';
+// import Header from '../header';
 const ServiceItem= (props) => {
-    const {list_of_titles,list_of_items,heading} = props;
+    const {list_of_titles,list_of_items,heading,list_of_prices} = props;
     return(
         <View style={styles.bigcontainer}>
-            <Header></Header>
+            {/* <Header></Header> */}
             <View style = {styles.headingbox}><Text style={styles.heading}>{heading}</Text></View>
             
             {list_of_titles.map((item,key) => (<View key={key} style = {styles.Container}>
                     <View style={styles.textbox}><Text style={styles.text}>{item}</Text></View>
                     <View style = {styles.items}>
                     {list_of_items[key].map((item1,key1) => ( <View key ={key1}>
-                        <ServiceItemType type={item1} price="10"></ServiceItemType>
+                        <ServiceItemType type={item1} price={list_of_prices[key][key1]}></ServiceItemType>
                         </View>))}
                     </View>
             {/* <Footer></Footer> */}
