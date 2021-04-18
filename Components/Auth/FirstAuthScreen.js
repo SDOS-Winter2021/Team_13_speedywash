c4a10943acbd01d15ec5cb70ecdc522103019437
aman18327 <aman18327@iiitd.ac.in>
2021-04-18 14:32:40 +0530

    Enhanced UI of SignIn Screen Fixed #49

--- Components/Auth/FirstAuthScreen.js
+++ Components/Auth/FirstAuthScreen.js
@@ -4,7 +4,9 @@
 import React from 'react'
 import { FontAwesome, MaterialIcons, AntDesign  } from '@expo/vector-icons';
 import { Text, View, Button, TouchableOpacity } from 'react-native';
-
+import { Image, StyleSheet } from 'react-native';
+import keys from "../../configs/KEYS";
+import { ceil } from 'react-native-reanimated';
 /**
  * 
  * @param {Object} obj - An object
@@ -13,10 +15,34 @@ import { Text, View, Button, TouchableOpacity } from 'react-native';
  */
 function FirstAuthScreen({ setCurrentStage }) {
     return (
-        <View>
-            <Button title="Sign in" onPress={() => setCurrentStage(1)} />
+        <View style={styles.background}>
+            <View style={styles.container}>
+                <Image 
+                style={styles.logo} 
+                source={require("../../assets/logo.png")} />
+            </View>
+            <View style={{alignSelf: "center", margin: "10%"}}>
+                <Button title="Sign in" onPress={() => setCurrentStage(1)} />
+            </View>
         </View>
     )
 }
-
+const styles = StyleSheet.create({
+    background: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    container : {
+        alignSelf: 'center',
+        marginHorizontal: "10%"
+    },
+    logo : {
+        // width: "50%",
+        // height: undefined,
+        // aspectRatio: 2/1
+        alignSelf: "center",
+        margin: "10%"
+    }
+});
 export default FirstAuthScreen
